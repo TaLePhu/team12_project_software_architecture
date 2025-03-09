fa973502b3c1b608eb8b5130d51118a3fba545d7
TaLePhu <lephu18062@gmail.com>
2025-03-09 16:00:48 +0700

    khoi tao du an

---
+++ src/main/java/iuh/se/team/webbookstore_backend/WebbookstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package iuh.se.team.webbookstore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbookstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbookstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/se/team/webbookstore_backend/WebbookstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.se.team.webbookstore_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbookstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
