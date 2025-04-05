9516d11743ded3ebc871c37d91b8430b9a26ec7b
LAPTOP-LVT6JQM0\nguye <caotri2000mwg@gmail.com>
2025-04-05 10:34:29 +0700

    add BookController v2

--- src/main/java/iuh/se/team/webbookstore_backend/config/WebConfig.java
+++ src/main/java/iuh/se/team/webbookstore_backend/config/WebConfig.java
@@ -1,19 +1,19 @@
-//package iuh.se.team.webbookstore_backend.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        // Cho phép tất cả các domain truy cập
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3001")  // Chỉ cho phép truy cập từ frontend tại localhost:3001
-//                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP được phép
-//                .allowedHeaders("*"); // Cho phép tất cả các header
-//    }
-//}
+package iuh.se.team.webbookstore_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Cho phép tất cả các domain truy cập
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3001")  // Chỉ cho phép truy cập từ frontend tại localhost:3001
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP được phép
+                .allowedHeaders("*"); // Cho phép tất cả các header
+    }
+}
