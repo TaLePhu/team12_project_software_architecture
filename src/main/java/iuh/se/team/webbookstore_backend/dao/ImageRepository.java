7799cec18f2b77866bea7b5bd05aeed520acf712
LAPTOP-LVT6JQM0\nguye <caotri2000mwg@gmail.com>
2025-04-05 10:20:54 +0700

    add BookController

--- src/main/java/iuh/se/team/webbookstore_backend/dao/ImageRepository.java
+++ src/main/java/iuh/se/team/webbookstore_backend/dao/ImageRepository.java
@@ -1,10 +1,14 @@
 package iuh.se.team.webbookstore_backend.dao;
 
+import iuh.se.team.webbookstore_backend.entities.Book;
 import iuh.se.team.webbookstore_backend.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "images")
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+    List<Image> findByBook(Book book);
 }
