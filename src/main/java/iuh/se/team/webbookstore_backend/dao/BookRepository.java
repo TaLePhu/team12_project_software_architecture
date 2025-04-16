91d89e5edcf199340b2698e0c43e01b2885f6622
TaLePhu <lephu18062@gmail.com>
2025-04-16 16:54:14 +0700

    add findByBookNameContaining

--- src/main/java/iuh/se/team/webbookstore_backend/dao/BookRepository.java
+++ src/main/java/iuh/se/team/webbookstore_backend/dao/BookRepository.java
@@ -1,11 +1,15 @@
 package iuh.se.team.webbookstore_backend.dao;
 
 import iuh.se.team.webbookstore_backend.entities.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RepositoryRestResource(path = "books")
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    Page<Book> findByBookNameContaining(@RequestParam("bookName") String bookName, Pageable pageable);
 }
