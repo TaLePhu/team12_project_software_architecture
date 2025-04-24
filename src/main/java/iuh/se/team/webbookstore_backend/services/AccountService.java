c502b95db5a97e91045d7f45dfdbd6c3af281c28
TaLePhu <lephu18062@gmail.com>
2025-04-24 16:18:07 +0700

    encode user password by bCryptPasswordEncoder

--- src/main/java/iuh/se/team/webbookstore_backend/services/AccountService.java
+++ src/main/java/iuh/se/team/webbookstore_backend/services/AccountService.java
@@ -4,6 +4,7 @@ import iuh.se.team.webbookstore_backend.dao.UserRepository;
 import iuh.se.team.webbookstore_backend.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,9 @@ public class AccountService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public ResponseEntity<?> signUp(User user) {
         if(userRepository.existsByUsername(user.getUsername())) {
             return ResponseEntity.badRequest().body("Username already exists");
@@ -19,6 +23,11 @@ public class AccountService {
             return ResponseEntity.badRequest().body("Email already exists");
         }
 
+        //encode user password
+        String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        //save user to database
         User newUser = userRepository.save(user);
         return ResponseEntity.ok("signUp successful");
     };
